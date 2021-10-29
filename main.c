9ba1b3c2f413900deb0c62f53fa2fa772fe331ae
Guibson1 <guibsonsilvalima@gmail.com>
2021-10-28 22:08:51 -0300

    Testes

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main() {
+    int valor1, valor2, soma = 0;
+
+    valor1 = 2;
+    valor2 = 78;
+    soma = valor1 + valor2;
+
+
+    printf(" Soma = ");
+    printf("%d",soma);
+ 
+}
